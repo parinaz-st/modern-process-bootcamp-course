4bf0410439a699029deba9356c4158c1de492b0c
parinaz <p.sattarzadeh@gmail.com>
2022-04-23 22:54:06 +0430

    ویدئو 3 jpa

--- spring-persistence-module/spring-jpa/src/main/java/com/modern/process/BankApplication.java
+++ spring-persistence-module/spring-jpa/src/main/java/com/modern/process/BankApplication.java
@@ -1,13 +1,24 @@
 package com.modern.process;
 
+import com.modern.process.domain.Post;
+import com.modern.process.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BankApplication {
+public class BankApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankApplication.class, args);
 	}
-
+	@Autowired
+	private PostRepository postRepository;
+	@Override
+	public void run(String... args) throws Exception {
+		Post post= new Post();
+		post.setTitle("post_1");
+		postRepository.save(post);
+	}
 }
