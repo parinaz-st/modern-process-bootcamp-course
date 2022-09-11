63d49eb96d004ad58e0381376d82c27e94b70e0a
p_sattarzadeh <p_sattarzadeh@modernisc.com>
2022-09-11 08:46:47 +0430

    spring transaction- done!

--- spring-boot-module/spring-transaction/src/main/java/com/modern/transaction/TransactionApplication.java
+++ spring-boot-module/spring-transaction/src/main/java/com/modern/transaction/TransactionApplication.java
@@ -1,13 +1,27 @@
 package com.modern.transaction;
 
+import com.modern.transaction.model.Account;
+import com.modern.transaction.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class TransactionApplication {
+import java.math.BigDecimal;
 
+@SpringBootApplication
+public class TransactionApplication implements CommandLineRunner {
+
+
+	@Autowired
+	private AccountRepository accountRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(TransactionApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		accountRepository.save(new Account("user1", new BigDecimal(1000)));
+		accountRepository.save(new Account("user2", new BigDecimal(2000)));
+	}
 }
