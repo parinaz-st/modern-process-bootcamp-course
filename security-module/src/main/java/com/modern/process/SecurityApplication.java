bd6d880bfc64fc1d81e984154a4868c67076b76c
p_sattarzadeh <p_sattarzadeh@modernisc.com>
2022-09-21 09:35:42 +0430

    Spring security module added

---
+++ security-module/src/main/java/com/modern/process/SecurityApplication.java
@@ -0,0 +1,11 @@
+package com.modern.process;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class);
+    }
+}
